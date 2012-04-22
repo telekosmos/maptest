fd03462ac48a65851b03fd8deb886096d74ddc81
telekosmos <telekosmos@zoho.com>
2012-04-22 12:16:56 +0200

    Map bad, commit to be able to create new branch...

--- js/list-tpl.js
+++ js/list-tpl.js
@@ -1,14 +1,31 @@
-
 $(document).ready(function () {
-console.info ("Starting...")
+  console.info("Starting...")
   var listTpl = 'Client List <div class="max-height-200 scrollable" id="list">' +
-		'<table class="table table-striped">' +
+    '<table class="table table-striped">' +
     '{{#clients}}<tr><td>{{name}} ({{loc}})</td></tr>{{/clients}}' +
-  	'</table></div>'
+    '</table></div>'
 
   var output = Mustache.render(listTpl, clients);
-  $('#clientListDiv').remove ('table')
+  $('#clientListDiv').remove('table')
   $('#clientListDiv').append(output)
 
-  $('#gmap3').gmap3()
+  $('#gmap3').gmap3({
+    action:'addMarker',
+//    address:"place de l'étoile, paris",
+    address: "casiano martínez 39, vigo, spain",
+    map:{
+      center:true,
+      zoom:14
+    },
+    marker:{
+      options:{
+        draggable:true
+      }
+    },
+    infowindow:{
+      options:{
+        content:'I live here !'
+      }
+    }
+  })
 })
